e6a759906d054bf287f5bd2f2b0eda5ddf50bf59
Matheus Barão Fiorin <matheusfiiorin@gmail.com>
2023-03-21 11:32:08 -0300

    Update ExceptionHandler methods

--- src/exceptions/handler.ts
+++ src/exceptions/handler.ts
@@ -2,15 +2,23 @@ import { NextFunction } from "express";
 import Exception from './exception';
 
 export class ExceptionHandler {
-  static badRequest(next: NextFunction, customMessage?: string) {
-    next(new Exception(400, customMessage || 'Bad request.'));
+  static createException(status: number, message: string): Exception {
+    return new Exception(status, message);
   }
 
-  static unauthorized(next: NextFunction, customMessage?: string) {
-    next(new Exception(401, customMessage || 'Unauthorized access.'));
+  static badRequest(next: NextFunction, customMessage?: string): void {
+    next(this.createException(400, customMessage || 'Bad request.'));
   }
 
-  static internalServerError(next: NextFunction, customMessage?: string) {
-    next(new Exception(500, customMessage || 'Internal server error.'));
+  static unauthorized(next: NextFunction, customMessage?: string): void {
+    next(this.createException(401, customMessage || 'Unauthorized access.'));
+  }
+
+  static notFound(next: NextFunction, customMessage?: string): void {
+    next(this.createException(404, customMessage || 'Not found.'));
+  }
+
+  static internalServerError(next: NextFunction, customMessage?: string): void {
+    next(this.createException(500, customMessage || 'Internal server error.'));
   }
 }
